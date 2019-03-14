ae2c93bff5123a3e71e799577cae425dc0a22fda
Dennis Dang <dangggdennis@gmail.com>
2019-03-14 00:45:05 -0700

    Count bits, find parity of bit-word, swap bits

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,30 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    # Method 1: Brute force
+    # result = 0
+    # while x:
+    #     result ^= x & 1
+    #     x >> 1
+    # return result
+
+    # Method 2: Slightly faster version of brute force by dropping the least set bit
+    # O(k) where k is the number of bits set to 1
+    # result = 0
+    # while x:
+    #     result ^= 1
+    #     x &= x-1
+    # return result
+
+    # Method 3: cache and abuse XOR property of being associative
+    # Assume working with 8-bit word
+    # x =^ x >> 32
+    # x =^ x >> 16
+    # x =^ x >> 8
+    # x =^ x >> 4
+    # x =^ x >> 2
+    # x =^ x >> 1
+    # return x & 0x1
 
 
 if __name__ == '__main__':
--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def swap_bits(x, i, j):
     # TODO - you fill in here.
-    return 0
+    max_bit = x >> i & 1
+    least_bit = x >> j & 1
+
+    if max_bit != least_bit:
+        bit_mask = (1 << i) | (1 << j)
+        print('x', x)
+        print('bitmask', bit_mask)
+        return x ^ bit_mask
+
+    return x
 
 
 if __name__ == '__main__':
